f169230da3fc011d5c01336896a2d520d34ce74c
Riley Schippers <02riley.schippers@gulllakecs.org>
2019-02-06 13:02:15 -0500

    Added a Greetings Function

---
+++ Functions-Git.py
@@ -0,0 +1,11 @@
+# 4.13.3: Greetings
+# Riley Schippers
+# 2.5.19
+
+name = input(" What is your name???: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you")
+
+greeting() 
