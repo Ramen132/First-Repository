cf28b49b6587b3f2ff2f643d5567943f242ec162
Riley Schippers <02riley.schippers@gulllakecs.org>
2019-02-11 13:00:46 -0500

    Add the Functions & Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,11 +1,25 @@
 # 4.13.3: Greetings
 # Riley Schippers
 # 2.5.19
-
+'''
 name = input(" What is your name???: ")
 
 def greeting():
     print("Hi there " + name + "!")
     print("Nice to meet you")
 
-greeting() 
+greeting()
+'''
+
+# 4.13.4: Functions and Variables
+# Riley Schippers
+# 2.11.19
+
+x = 406
+
+def print_something():
+    x = 3
+    print  ('\n', x)
+
+print('\n', x)
+print_something()
